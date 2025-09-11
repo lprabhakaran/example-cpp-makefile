4a8f690a3f3b65828f875724315ba533f8b099b7
Lekha Prabhakaran <168733991+lprabhakaran@users.noreply.github.com>
2025-09-11 20:43:14 +0530

    Clarify command injection vulnerability in example

--- project/src/command_injection.cpp
+++ project/src/command_injection.cpp
@@ -1,11 +1,12 @@
-// command_injection.cpp
 #include <cstdlib>
+#include <iostream>
+#include <cstdio>
 
 void commandInjectionExample() {
     char userInput[100];
     std::cout << "Enter filename to list: ";
     std::cin >> userInput;
     char command[200];
-    sprintf(command, "ls %s", userInput);  // User could inject ; rm -rf /
+    sprintf(command, "ls %s", userInput);  // Vulnerable: command injection
     system(command);
 }
